09e2b427c410031814e1a7939d2d86bd34d62e96
kakinouchi <32264080+kakinouchi@users.noreply.github.com>
2021-12-07 23:54:25 +0900

    Update IO-cheat-sheet.clj

--- IO-cheat-sheet.clj
+++ IO-cheat-sheet.clj
@@ -1,2 +1,12 @@
-;; Read multi lines
+;; Read multi lines.
 (def s (line-seq (java.io.BufferedReader. *in*)))
+
+;; or
+(def foo [])
+
+(doseq [_ (range some-count)]
+    (def foo 
+      (conj foo
+            (Integer/parseInt (clojure.string/trim (read-line)))
+            ))
+)
