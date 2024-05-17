ba81fbcc25a0efd5f69c2c3cfe28aac9dd06d461
kakinouchi <32264080+kakinouchi@users.noreply.github.com>
2024-05-17 10:25:42 +0900

    Update InsertANodeAtTheHeadOfALinkedList.js

--- Data Structures/Linked Lists/InsertANodeAtTheHeadOfALinkedList.js
+++ Data Structures/Linked Lists/InsertANodeAtTheHeadOfALinkedList.js
@@ -1,2 +1,19 @@
 // (2024年5月13日)
 // 問題文があまりピンと来ずに今日は終了...
+
+// (2024年5月16日)
+// 図に書いてみた：
+// https://github.com/kakinouchi/HackerRank/blob/master/Data%20Structures/Linked%20Lists/InsertANodeAtTheHeadOfALinkedList_1.jpg
+// https://github.com/kakinouchi/HackerRank/blob/master/Data%20Structures/Linked%20Lists/InsertANodeAtTheHeadOfALinkedList_2.jpg
+
+// (2024年5月17日)
+// できた！ なんてことはなかった。
+
+function insertNodeAtHead(head, data) {
+    const newNode = new SinglyLinkedListNode();
+    
+    newNode.data = data;
+    newNode.next = head;
+        
+    return newNode;
+}
