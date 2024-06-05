83c904cd373ea34d1411afbb0757e2a8d22ad3f2
kakinouchi <32264080+kakinouchi@users.noreply.github.com>
2024-06-05 15:10:50 +0900

    Update InsertANodeAtASpecificPositionInALinkedList.js

--- Data Structures/Linked Lists/InsertANodeAtASpecificPositionInALinkedList.js
+++ Data Structures/Linked Lists/InsertANodeAtASpecificPositionInALinkedList.js
@@ -141,4 +141,79 @@ function insertNodeAtPosition(llist, data, position) {
     _insertNodeAtPosition(llist, data, position, llist);
 }
 
+// (2024年6月5日)
+// あー、「既存のリストのつなぎ方を変える」という発想でやってたけど、
+// 発想を変えて、
+// 「リストをゼロから作り直す」
+// とすればよさそうだな。
+// そうすると再帰で書きやすそう。
+// 8行目の書き方で、なるべく position の数字によらず同じ書き方にできないかなーと考えてたら
+// この発想にいたった。
+
+function insertNodeAtPosition(llist, data, position) {
+
+    const recursiveInsertNodeAtPosition = (llist, count = 0) => {
+        console.log("count");
+        console.log(count)
+        console.log(llist.data);
+                
+        const nextNode = llist.next;
+        
+        if(nextNode == null){
+            return;
+        }
+        return recursiveInsertNodeAtPosition(nextNode, count + 1)
+    }
+    
+    recursiveInsertNodeAtPosition(llist);
+    // if you want to insert the data to the position = 0
+    // const newNode = new SinglyLinkedListNode();
+    // newNode.data = data;
+    // newNode.next = llist;
+    // const headNode = newNode;
+    
+    // return headNode;
+    
+    // if you want to insert the data to the position = 1
+    // const firstNodeRef = llist;
+    // const secondNodeRef = llist.next;
+    // const newNode = new SinglyLinkedListNode();
+    // newNode.data = data;
+    // newNode.next = secondNodeRef;
+    // firstNodeRef.next = newNode
+    
+    // return llist;
+    // const newNode = new SinglyLinkedListNode();
+    // newNode.data = data;
+    // newNode.next = llist;
+    // const headNode = newNode;
+    
+    // return headNode;
+    // let currentNode = llist;
+    // let count = 0;
+    // let newLList = null;
+    // while( currentNode != null ) {
+    //     console.log()
+    //     // if( count == position) {
+    //     //     const newNode = new SinglyLinkedListNode();
+    //     //     newNode.data = data;
+    //     //     newNode.next = 
+    //     // } else {
+    //     //     const newNode = new SinglyLinkedListNode();
+            
+    //     // }
+    //     currentNode = currentNode.next;
+    // }
+
+
+    // if you want to insert the data to the position = 2
+    // const secondNodeRef = llist.next;
+    // const thirdNodeRef = llist.next.next;    
+    // const newNode = new SinglyLinkedListNode();
+    // newNode.data = data;
+    // newNode.next = thirdNodeRef;
+    // secondNodeRef.next = newNode
+    
+    // return llist;
+}
 
